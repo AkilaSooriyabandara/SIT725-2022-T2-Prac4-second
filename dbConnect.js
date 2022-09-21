0c353555caee813fca3710c94b396b635c980f91
Akila Sooriyabandara <109722820+AkilaSooriyabandara@users.noreply.github.com>
2022-09-21 23:36:17 +1000

    updates -edited test.js -editedserver.js

--- dbConnect.js
+++ dbConnect.js
@@ -1,20 +1,20 @@
-require('dotenv').config()
-const MongoClient = require('mongodb').MongoClient;
+// require('dotenv').config()
+// const MongoClient = require('mongodb').MongoClient;
  
-//Database connection
+// //Database connection
 
-const uri = 'mongodb+srv: //Akila:<Sooriyabandara123>@cluster0.rumcwol.mongodb.net/test'
-const client =  new MongoClient(uri,{ useNewUrlParser: true })
+// const uri = 'mongodb+srv: //Akila:<Sooriyabandara123>@cluster0.rumcwol.mongodb.net/test'
+// const client =  new MongoClient(uri,{ useNewUrlParser: true })
 
 
-client.connect((err,db) => {
-            if(!err) {
-                console.log('MongoDB Connected')
-            }
-            else {
-                console.log("DB Error: ", err);
-                process.exit(1);
-            }
-        })
+// client.connect((err,db) => {
+//             if(!err) {
+//                 console.log('MongoDB Connected')
+//             }
+//             else {
+//                 console.log("DB Error: ", err);
+//                 process.exit(1);
+//             }
+//         })
     
-    module.MongoClient = client;
+//     module.MongoClient = client;
