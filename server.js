90ac7805c42ab93cf7ae1ba91977128ba298c868
Akila Sooriyabandara <109722820+AkilaSooriyabandara@users.noreply.github.com>
2022-09-26 18:24:27 +1000

    updated the server.js, index.html and scripts.js

--- server.js
+++ server.js
@@ -18,8 +18,21 @@ app.get('/addTwoNumbers/n1/n2', function(request, response){
     response.sendStatus(200);
 
 })
+
+let io = require('socket.io')(http);
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  setInterval(()=>{
+    socket.emit('number', parseInt(Math.random()*10));
+  }, 1000);
+
+});
+
 varport = process.env.port || 3000;
 
-app.listen(port,()=>{
+htpp.listen(port,()=>{
     console.log("App running at http://localhost:"+port)
 })
